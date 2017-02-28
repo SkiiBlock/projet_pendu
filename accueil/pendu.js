6668feb2fe9dbb9760af639466eff6b3cb166088
remyoger <remyoger@gmail.com>
2017-02-28 09:57:15 +0100

    28/02/17
    Disparition des boutons

--- accueil/pendu.js
+++ accueil/pendu.js
@@ -1,9 +1,33 @@
-/*function test() {
+	console.log("Chargement de Js : OK "); //Test du JavaScript
+
+function supprimer(supp, objet_id){  
 	
-	var div = document.getElementById("themesDifficulte1");
-	var bouton = CreateElement.......(input)
+ 	var elem=document.getElementById(supp);
+	//obj.style.width = '0px';
+	elem.style.display = 'none';
 	
+	var elem=document.getElementById(supp+1)
+	elem.style.display = 'none';
 	
+	var elem=document.getElementById(supp+2)
+	elem.style.display = 'none';
+	
+	afficher(objet_id);
+}
+
+function afficher(objet_id){
+	
+	console.log("Le niveau de difficulté est : " + objet_id);
 	
 }
+
+
+	
+	
+	
+	/*if (obj.style.display == 'inline-block') {
+		obj.style.display='none'
+	}else{
+		obj.style.display='block'
+	}*/
 
