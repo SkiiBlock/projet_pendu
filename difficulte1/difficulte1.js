ed6d6ad7a32a56f5648680c008bd6e0ec36705f0
SkiiBlock <antoine.zalewski59@hotmail.fr>
2017-02-28 08:01:59 +0100

    Add files via upload

---
+++ difficulte1/difficulte1.js
@@ -0,0 +1,29 @@
+function requestXML(url, categorie) { //fonction => request un mot dans le XML
+    
+    var xhttp = new XMLHttpRequest();
+    
+    xhttp.onreadystatechange = function() {
+        
+        console.log(this.readyState);
+        console.log(this.status);
+        
+        if (this.readyState == 4 && this.status == 200) {
+            reponseXML(this, categorie);
+        }
+    }
+    xhttp.open('GET', url, true);
+    xhttp.send();
+}
+
+function reponseXML(xhttp, categorie) {
+    
+    var i = Math.floor(Math.random() * 6)
+    var xml = xhttp.responseXML.getElementsByTagName(categorie)[0].getElementsByTagName('mot')[i].childNodes[0].nodeValue;
+    
+    console.log(xml);
+    
+    var mot = xml.split('');
+    
+    console.log(mot);
+    
+}
