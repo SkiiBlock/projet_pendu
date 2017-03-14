2eb498d559d3bda1dd8ca48e0ed280b89c812a75
SkiiBlock <antoine.zalewski59@hotmail.fr>
2017-03-14 09:58:58 +0100

    Modification lecture fichier .xml // Modification recherche lettre

--- difficulte1/difficulte1.js
+++ difficulte1/difficulte1.js
@@ -1,4 +1,7 @@
-window.onload = function init() {                       // Création alphabet
+var response;
+var count = 0;
+
+window.onload = function () {                       // Création alphabet
     
     var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     
@@ -17,12 +20,41 @@ window.onload = function init() {                       // Création alphabet
         colonne.innerHTML += alphabet[i];
     }
     
-    var response;
+    requestText('apple.txt');
+    
+    /*var ua = navigator.userAgent;
+    var x = ua.indexOf('Chrome');
+    var y = ua.indexOf('Firefox');
+    
+    if (x != -1) {
+        requestXML('difficulte1.xml');
+    }
+    
+    if (y != -1) {
+        requestText('apple.txt');
+    }*/
 }
 
-function requestXML(url) {                  // Création + envoie requête XML pour récupération des entrées
+function requestXML(url) {
     
     var xhttp = new XMLHttpRequest();
+    var categorie = document.getElementById('info').innerHTML;
+    
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            reponseXML(xhttp, categorie);
+        }
+    }
+    
+    xhttp.open('GET', url, true);
+    xhttp.send();
+    
+}
+
+function requestText(url) {
+    
+    var xhttp = new XMLHttpRequest();
+    var categorie = document.getElementById('info').innerHTML;
     
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -35,6 +67,20 @@ function requestXML(url) {                  // Création + envoie requête XML p
     
 }
 
+function reponseXML(xhttp, categorie) {
+      
+    var i = Math.floor(Math.random() * 6)
+    var xml = xhttp.responseXML.getElementsByTagName(categorie)[0].getElementsByTagName('mot')[i].childNodes[0].nodeValue;
+     
+    var mot = xml.split('');
+    response = mot;
+     
+    console.log(mot);
+    
+    tableau(mot);
+      
+}
+
 function responseText(xhttp) {              // Récupération de toutes les entrées possibles + choix aléatoire
     
     var text = xhttp.responseText.split('\n');
@@ -44,7 +90,7 @@ function responseText(xhttp) {              // Récupération de toutes les entr
     text[node] = text[node].trim();
     
     response = text[node].split('');
-    
+    console.log(response);
     tableau(response);
     
 }
@@ -57,27 +103,69 @@ function tableau(mot) {           // Création tableau + affichage dans tableau
     
     for (var i=0; i<mot.length; i++) {
         var colonne = ligne.insertCell(i);
-        colonne.innerHTML = ' ';
+        colonne.innerHTML = '';
     }
 }
 
-function core() {                           // Recherche lettre
+function core() {                   // Recherche lettre
     
     var x = event.target.innerHTML;
     console.log(x);
     
-    for (var i=0; i<response.length; i++) {
+     /*for (var i=0; i<response.length; i++) {
         var y = response.indexOf(x, i);
+        console.log(y);
+        
+        if (y == -1) {
+            suppressionLettre(x);
+        }else{
+            affichageTableau(y);
+            suppressionLettre(x);
+        }
+        
+    }*/
+    
+    var z = 0;
+    var i = 0;
+    
+    while (i < response.length) {
+        i++
+        var y = response.indexOf(x, z);
         
         if (y != -1) {
-            affichageTableau(y);
+            affichageTableau(x);
+            suppressionLettre(y);
+            z = y;
         }
+        
+        if (y == -1) {
+            suppressionLettre(y);
+        }
+    }    
+}
+
+function affichageTableau(y) {          // Affichage lettre dans 'affichageMot' + win
+    
+    var tableau = document.getElementById('affichageMot').getElementsByTagName('td');
+    console.log(tableau.length);
+    console.log(response);
+    tableau[y].innerHTML = response[y];
+    count++;
+    console.log(count);
+    if (count == tableau.length) {
+        console.log('GAGNE');
     }
+    
     
 }
 
-function affichageTableau(y) {              // Affichage lettre
-    console.log(response);
-    var tableau = document.getElementById('affichageMot').getElementsByTagName('td');
-    tableau[y].innerHTML = response[y];
+function suppressionLettre(x) {                 // Suppresion lettre dans 'table'
+    
+    var tableau = document.getElementById('table').getElementsByTagName('td');
+    
+    for (var i=0; i<tableau.length; i++) {
+        if (tableau[i].innerHTML == x) {
+            tableau[i].innerHTML = ' ';
+        }
+    }
 }
