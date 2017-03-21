f4eb231e26054eb77d5ab45b392e48494fff65a3
SkiiBlock <antoine.zalewski59@hotmail.fr>
2017-03-21 09:18:57 +0100

    Update difficulte1.js

--- difficulte1/difficulte1.js
+++ difficulte1/difficulte1.js
@@ -24,12 +24,12 @@ window.onload = function () {                       // Création alphabet
     var y = ua.indexOf('Firefox');
     
     if (x != -1) {
-        console.log('requestXML');
+        //console.log('requestXML');
         requestXML('difficulte1.xml');
     }
     
     if (y != -1) {
-        console.log('requestText');
+        //console.log('requestText');
         requestText('apple.txt');
     }
 }
@@ -73,6 +73,8 @@ function reponseXML(xhttp, categorie) {
      
     var mot = xml.split('');
     response = mot;
+     
+    //console.log(mot);
     
     tableau(mot);
     
@@ -87,6 +89,7 @@ function responseText(xhttp) {              // Récupération de toutes les entr
     text[node] = text[node].trim();
     
     response = text[node].split('');
+    //console.log(response);
     tableau(response);
     
 }
@@ -106,9 +109,11 @@ function tableau(mot) {           // Création tableau + affichage dans tableau
 function core() {                   // Recherche lettre
     
     var x = event.target.innerHTML;
+    //console.log(x);
     
      for (var i=0; i<response.length; i++) {
         var y = response.indexOf(x, i);
+        //console.log(y);
         
         if (y == -1) {
             suppressionLettre(x);
@@ -123,18 +128,25 @@ function core() {                   // Recherche lettre
 function affichageTableau(y) {          // Affichage lettre dans 'affichageMot' + win
     
     var tableau = document.getElementById('affichageMot').getElementsByTagName('td');
+    //console.log(tableau.length);
     tableau[y].innerHTML = response[y];
     
     var z = [];
     for (var i=0; i<tableau.length; i++) {
         z.push(tableau[i].innerHTML);     
     }
+    
+    //console.log(z);
+    
+    if (z == response) {
+        //console.log('Gagne');
+    }
         
-    for (var i=0; i<z.length; i++) {
+    /*for (var i=0; i<z.length; i++) {
         if (z.indexOf('', i) == -1) {
             console.log('GAGNE');
         }
-    }
+    }*/
         
 }
 
