863b443e1100bdd8ff1d4059f2909b4f5211c144
SkiiBlock <antoine.zalewski59@hotmail.fr>
2017-05-21 20:20:31 +0200

    Fix #2 tirage + commentaires

--- difficulte1/difficulte1.js
+++ difficulte1/difficulte1.js
@@ -1,4 +1,5 @@
-var response;       // création des variables globales nécessaire pour plusieurs fonctions
+    /**** VARIABLES GLOBALES ****/
+var response;
 var coups = 0;
 var pinTable = [];
 var motTire = [];
@@ -7,53 +8,55 @@ var bResponse = 0;
 var interval = null;
 var corrige = [];
 
-window.onload = load();
+window.onload = load();     // fonction lancé au chargement de la page
     
 function load() {       // fonction utilisé pour initialiser le code et l'affichage
     
     /**** CREATION D'UN ALPHABET ****/
-    
-    var tableau = document.getElementById('table');
+    var tableau = document.getElementById('table');     // on récupère l'ID du tableau
     
     var ligne = tableau.insertRow(-1);      // insertRow() permet d'insérer une ligne
-    for (var i=0; i<13; i++) {              // on ne met que les 13 premières lettres
+    for (var i=0; i<13; i++) {
         var colonne = ligne.insertCell();   // insertCell() permet d'insérer une colonne
         colonne.innerHTML += alphabet[i];
     }
     
     var ligne = tableau.insertRow(-1);      // on recommence pour une nouvelle ligne
-    for (var i=13; i<26; i++) {             // on part maintenant de la 14e lettre
+    for (var i=13; i<26; i++) {
         var colonne = ligne.insertCell();
         colonne.innerHTML += alphabet[i];
     }
     
     /**** TEST DU NAVIGATEUR ****/
+    if (motTire != 20) {
+        var ua = navigator.userAgent;           // userAgent nous renvoie des informations sur le navigateur utilisé, stocké dans la variable 'ua'
+        var x = ua.indexOf('Chrome');           // on cherche quel est le navigateur utilisé
+        var y = ua.indexOf('Firefox');
     
-    var ua = navigator.userAgent;           // userAgent nous renvoie des informations sur le navigateur utilisé, stocké dans la variable 'ua'
-    var x = ua.indexOf('Chrome');           // on cherche quel est le navigateur utilisé
-    var y = ua.indexOf('Firefox');
+        if (x != -1) {                          // indexOf() renvoie -1 lorsqu'il ne trouve pas le mot recherché (ici 'Chrome' ou 'Firefox')
+            requestXML('difficulte1.xml');
+        }
     
-    if (x != -1) {                          // indexOf() renvoie -1 lorsqu'il ne trouve pas le mot recherché (ici 'Chrome' ou 'Firefox')
-        requestXML('difficulte1.xml');
+        if (y != -1) {
+            var categorie = document.getElementById('info').innerHTML;
+            requestText(categorie+'.txt');
+        }
     }
     
-    if (y != -1) {
-        var categorie = document.getElementById('info').innerHTML;
-        requestText(categorie+'.txt');
-    }
+    /**** AFFICHAGE EMPLACEMENT IMAGE ****/
+    var image = new Image();        // on créé une nouvelle image js 
+    image.src = '../Image/reset.png';
     
-    var image = new Image();
     image.addEventListener('load', function() {
-        var reduction = 9;
-        var img = document.getElementById('img');
-        var div = document.getElementById('div0');
-
-        img.src = image.src;
+        var reduction = 9;      // coefficient réducteur pour l'image
+        var img = document.getElementById('img');       // on récupère l'ID de l'image (html)
+        
+        img.src = image.src;        // on applique l'image, puis les réductions
         img.width = Math.round(image.width / reduction);
         img.height = Math.round(image.height / reduction);
     });
                 
-    image.src = '../Image/reset.png';
+    
 }
 
 function requestXML(url) {      /**** XMLHttpRequest (méthode #1) ****/
@@ -61,15 +64,14 @@ function requestXML(url) {      /**** XMLHttpRequest (méthode #1) ****/
     var xhttp = new XMLHttpRequest();                           // création de la requête
     var categorie = document.getElementById('info').innerHTML;  // on récupère la catégorie de la page chargé
     
+    xhttp.open('GET', url, true);       // on ouvre la requête
+    xhttp.send();                       // on envoie la requête
+    
     xhttp.onreadystatechange = function() {     // .onreadystatechange vérifie si les variables vont changer
         if (this.readyState == 4 && this.status == 200) {
             reponseXML(xhttp, categorie);       // on lance la fonction réponse quand les variables ont changé et que la requête a réussi
         }
-    }
-    
-    xhttp.open('GET', url, true);       // on ouvre la requête
-    xhttp.send();                       // on envoie la requête
-    
+    }    
 }
 
 function requestText(url) {     /**** XMLHttpRequest (méthode #2) ****/
@@ -77,70 +79,53 @@ function requestText(url) {     /**** XMLHttpRequest (méthode #2) ****/
     var xhttp = new XMLHttpRequest();
     var categorie = document.getElementById('info').innerHTML;
     
+    xhttp.open('GET', url, true);
+    xhttp.send();
+    
     xhttp.onreadystatechange = function() {     
         if (this.readyState == 4 && this.status == 200) {
             responseText(this);         // on change ici la fonction réponse car on récupère du texte
         }
     }
-    
-    xhttp.open('GET', url, true);
-    xhttp.send();
-    
 }
 
-function reponseXML(xhttp, categorie) {     /**** RECUPERATION REPONSE ****/
-      
-    var i = Math.floor(Math.random() * 20)       // on tire un nombre aléatoire entre 0 et le nombre de mot maximum
-    var xml = xhttp.responseXML.getElementsByTagName(categorie)[0].getElementsByTagName('mot')[i].childNodes[0].nodeValue;                  // on récupère un mot de la liste en fonction du nombre tiré
+function reponseXML(xhttp, categorie) {     /**** RECUPERATION REPONSE (méthode #1) ****/
+
+    var r = Math.floor(Math.random() * 20)       // on tire un nombre aléatoire entre 0 et le nombre de mot maximum
+    var xml = xhttp.responseXML.getElementsByTagName(categorie)[0].getElementsByTagName('mot')[r].childNodes[0].nodeValue;                  // on récupère un mot de la liste en fonction du nombre tiré
     
-    for (var i=0; i<motTire.length; i++) {
-        if (motTire[i] === xml) {
-            requestXML('difficulte1.xml');
-        }
+    if (motTire.indexOf(xml) != -1) {       // on vérifie si le mot a déjà été tiré
+        requestXML('difficulte1.xml')
+    }else{
+        motTire.push(xml);
     }
     
-    if (motTire.length == 20) {
+    if (motTire.length == 10) {                 // si la liste est de 10 mots, on lance la fin du jeu
         finDuJeu();
     }
     
-    if (motTire.length < 20) {
-        var y = motTire.indexOf(xml);
-        if (y == -1) {      // on enregistre le mot tiré uniquement s'il n'a pas déjà été tiré
-            motTire.push(xml);
-        }
-    }
-    
-    response = xml.split('');
+    response = xml.split('');       // on découpe le mot tiré
     tableau(response);       // on lance la fonction avec le mot tiré
 }
 
-function responseText(xhttp) {              /**** RECUPERATION REPONSE ****/
+function responseText(xhttp) {              /**** RECUPERATION REPONSE (méthode #2) ****/
     
-    var text = xhttp.responseText.split('\n');
-    xhttp.responseText = null;
-    
+    var text = xhttp.responseText.split('\n');  // on récupère la liste de mot    
     var i = Math.floor(Math.random()*text.length);
-    text[i] = text[i].trim();
+    var t = text[i].trim(); // .trim() supprime les espaces
     
     
-    for (var z=0; z<motTire.length; i++) {
-        if (motTire[z] === text[i]) {
-            requestText('ecole.txt');
-        }
+    if (motTire.indexOf(t) != -1) {
+        requestText(categorie+'.txt');
+    }else{
+        motTire.push(t);
     }
     
-    if (motTire.length == 2) {
+    if (motTire.length == 10) {                 // si la liste est de 10 mots, on lance la fin du jeu
         finDuJeu();
     }
-    
-    if (motTire.length != 2) {
-        var y = motTire.indexOf(text[i]);
-        if (y == -1) {
-            motTire.push(text[i]);
-        }
-    }
 
-    response = text[i].split('');
+    response = t.split('');
     tableau(response);
 }
 
@@ -156,58 +141,52 @@ function tableau(mot) {     /**** CREATION TABLEAU ****/
     }
 }
 
-function grabId(e) {
-    var e = window.event || e;              // on utilise deux méthodes pour récupérer l'élément cliqué
-	var targ = e.target || e.srcElement;
+function grabId() {    /**** RECUPERATION LETTRE ****/
+    var e = window.event || e;            // on utilise deux méthodes pour récupérer l'élément cliqué
+	var targ = e.target || e.srcElement;   // .target fait référence à l'objet
     
     var x = targ.innerHTML;
     rechercheLettre(x);    
 }
 
-window.onkeydown = function(e) {
-    var x = e.which;
-    var y = alphabet[(x-65)];
-    
+window.onkeydown = function(e) {        /**** RECUPERATION LETTRE (CLAVIER) ****/
+    var x = e.which;            // .which renvoie le code la touche
+    var y = alphabet[(x-65)];       // on adapte le code à la variable alphabet
     rechercheLettre(y);
 }
 
-function rechercheLettre(x) {
-    pinTable = [];
-    var a = response.indexOf(x, 0);
-    var b = 0;
+function rechercheLettre(x) {       /**** RECHERCHE LETTRE DANS MOT TIRE ****/
     
-    for (var i=0; i<26; i++) {
+    for (var i=0; i<26; i++) {      // on s'assure que le joueur a cliqué sur une lettre
         if (x == alphabet[i]) {
-            if (a == -1) {
-                suppressionLettre(x);
+            if (response.indexOf(x, 0) == -1) {              // si la lettre n'est pas dans le mot...
                 coups++;
+                if (coups == 9) {loose();}
                 
+                /**** AFFICHAGE IMAGE ****/
                 var image = new Image();
+                image.src = '../Image/' + coups + '.png';   // on affiche l'image en fonction du nombre de coups ratés
+                
                 image.addEventListener('load', function() {
                     var reduction = 9;
                     var img = document.getElementById('img');
-                    var div = document.getElementById('div0');
                     
                     img.src = image.src;
                     img.width = Math.round(image.width / reduction);
                     img.height = Math.round(image.height / reduction);
-                });
-                
-                image.src = '../Image/' + coups + '.png';
-                
-                if (coups == 9) {loose();}                
+                });                
             }
-    
-            while (b<response.length) {
-        
+            
+            var b = 0;
+            while (b<response.length) {     // la boucle while est importante : elle permet de trouver si la lettre tirée n'est pas plusieurs fois présente
                 var a = response.indexOf(x, b);
-        
-                if (a != -1) {
-                    pinTable.push(a);
-                    b = a + 1;
+                
+                if (a != -1) {          // si on trouve la lettre dans le mot...
+                    pinTable.push(a);   // .push(x) ajoute x à la fin du tableau
+                    b = a + 1;          // on s'assure que la recherche se relancera après la lettre trouvé
                 }
         
-                if (a == -1) {
+                if (a == -1) {              // si on ne trouve pas la lettre, on arrête la boucle
                     b = response.length;
                 }
             }
@@ -217,31 +196,29 @@ function rechercheLettre(x) {
     }
 }
 
-function affichageTableau() {          // Affichage lettre dans 'affichageMot' + win
+function affichageTableau() {       /**** AFFICHAGE LETTRE DANS TABLEAU ****/
 
     var c = 0;
-    
-    while (c<pinTable.length) {
+    while (c<pinTable.length) {     // la boucle while est importante : elle permet d'écrire plusieurs fois la lettre tirée si elle est plusieurs fois présente
         
         var x = pinTable[c];
         
         var tableau = document.getElementById('affichageMot').getElementsByTagName('td');
-        tableau[x].innerHTML = response[x];
-    
+        tableau[x].innerHTML = response[x];     // on écrit la lettre dans le tableau en fonction de sa place dans le mot
+        
+        /**** TEST MOT ****/
         var z = [];
-        for (var i=0; i<tableau.length; i++) {
-            z.push(tableau[i].innerHTML);     
+        for (var i=0; i<tableau.length; i++) {      // on écrit dans l'array z les lettres trouvées par l'utilisateur
+            z.push(tableau[i].innerHTML);
         }
     
-        var a = response.toString();
+        var a = response.toString();        // .toString() renvoie une chaîne de caractère
         var b = z.toString();
     
-        if (a == b) {
+        if (a == b) {       // on compare les deux mots
             win();
         }
-        
         c++;
-        
     }
 }
 
@@ -276,6 +253,10 @@ function win() {
     }, 2000);
     
     corrige.push('1');
+    
+    if (motTire.length == 20) {                 // si la liste est de 20 mots, on lance la fin du jeu
+        finDuJeu();
+    }
 }
 
 function loose() {
@@ -297,6 +278,10 @@ function loose() {
     }, 2000);
     
     corrige.push('0');
+    
+    if (motTire.length == 20) {                 // si la liste est de 20 mots, on lance la fin du jeu
+        finDuJeu();
+    }
 }
 
 function finDuJeu() {
@@ -304,7 +289,7 @@ function finDuJeu() {
     
     var textJS = document.getElementById('textJS');
     textJS.innerHTML = "Tu as terminé le thème " + x + '<br>';
-    textJS.innerHTML += "Tu as "+ bResponse +" bonnes réponses sur 20";
+    textJS.innerHTML += "Tu as "+ bResponse +" bonnes réponses sur 10";
     textJS.style.color = 'green';
     
     //console.log(motTire);
